d81f8d28d4cd06d691ba79dd6d15a3ef5a053722
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-24 23:43:52 +0530

    Create variable.php

---
+++ 20210421/variable.php
@@ -0,0 +1,10 @@
+<?php
+#https://paiza.io/projects/PHPH7Cmooas92uW-VlgD4w
+$txt1= "learn php ";
+$txt2= "(php.com)";
+$a= 28;
+$b=56;
+print  $txt1."\n";
+print "studying php at ".$txt2."\n";
+print $a*$b;
+?>
