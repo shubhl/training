d4b2c3619b39e4a51a404d37f9c7c463e47a9dbd
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 22:03:10 +0530

    Create array-scbasis.php

---
+++ 20210523/array-scbasis.php
@@ -0,0 +1,10 @@
+<?php
+#https://paiza.io/projects/Oy2tdFTZ83dKxY-3sKRdvQ
+ $colors= ["red","green","yellow","blue"];
+ 
+ echo $colors[0]."<br>";
+ echo $colors[1]."<br>";
+ echo $colors[2]."<br";
+ echo $colors[3];
+ 
+?>
