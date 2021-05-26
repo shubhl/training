3b8a2c1722db148344e07d4f27d47c2900b6ef2e
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 15:02:23 +0530

    Create function-withe-argu-x2.php

---
+++ 20210522/function-withe-argu-x2.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/S9-SMiwH2ZMS53TFwgcYuQ
+//function with  single argument
+    function value($name)
+    {
+        echo "$name good morning everyone","<br>";
+        echo "whaT IS ur name.";
+    }
+    
+    value ("shubham");
+?>
