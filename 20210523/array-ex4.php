3d5d07628a8ae2db234975ead7b1cf01fcf0608f
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 22:13:05 +0530

    Create array-ex4.php

---
+++ 20210523/array-ex4.php
@@ -0,0 +1,12 @@
+<?php
+//array
+#https://paiza.io/projects/jZWd1uHFTp5lkyH3uR0--Q another example
+
+    $colors[0]="red";
+    $colors[1]="yellow";
+    $colors[2]="green";
+    $colors[3]="blue";
+    
+ print_r($colors);
+
+?>
