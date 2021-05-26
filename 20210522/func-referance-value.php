187f618a25b8d42c082d217ac028edc0bf17ad49
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 16:53:29 +0530

    Create func-referance-value.php

---
+++ 20210522/func-referance-value.php
@@ -0,0 +1,14 @@
+<?php
+#https://paiza.io/projects/KhVN70PNHtBjcsj73VzeGg
+    function testinhg(&$a)//referance value
+    {
+     $a .=" good morning";   
+    }
+    $str ="hello";
+    testinhg($str);
+    
+    echo $str;
+    
+    
+    
+?>
