ebd9cb190edc02e8f4e4998b95ab28ad3120c084
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 22:00:26 +0530

    Create indexed-array.php

---
+++ 20210523/indexed-array.php
@@ -0,0 +1,10 @@
+<?php
+// Your code here!
+ $colors= array ("red","green","yellow","blue");
+ 
+ echo $colors[0]."<br>";
+ echo $colors[1]."<br>";
+ echo $colors[2]."<br";
+ echo $colors[3];
+ 
+?>
