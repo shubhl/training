e9f5d43bc018231a0b8bfe5ba69615d7eb3d399d
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 22:38:41 +0530

    Create indexed-array-unlist.php

---
+++ 20210523/indexed-array-unlist.php
@@ -0,0 +1,11 @@
+<?php
+//array with unlist tags
+#https://paiza.io/projects/JpjM-NNd7CusfBqwHkDH1A
+    $colors=array("red","green","yellow","blue");
+    echo "<ul>";
+    for ($i=0; $i < 4;$i++){
+    
+    echo "<li> $colors[$i] </li>";
+ }
+ echo "</ul>";
+?>
