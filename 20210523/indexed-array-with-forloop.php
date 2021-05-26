a55bbb186e3df0ff2136f248b41254ceaba2940c
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 22:27:34 +0530

    Create indexed-array-with-forloop.php

---
+++ 20210523/indexed-array-with-forloop.php
@@ -0,0 +1,10 @@
+<?php
+//array with for loop
+#https://paiza.io/projects/jZWd1uHFTp5lkyH3uR0--Q
+    $colors=array("red","green","yellow","blue");
+    
+    for ($i=0; $i < 4;$i++){
+    
+    echo $colors[$i],"<br>";
+}
+?>
