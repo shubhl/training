3237dd2e34f6fe748ee3277e170fb95193815c90
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-26 15:07:24 +0530

    Create function-with-two-argu.php

---
+++ 20210522/function-with-two-argu.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/qYDD2TmBCnUp2m06QRCt3g
+    function value($name1,$name2)
+    {
+        echo "my name is $name1 <br>";
+        echo "i am from $name2";
+    }
+    value("shubham","mumbai")
+    
+
+?>
