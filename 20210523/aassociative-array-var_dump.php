c02d90e7a03c7518e829fb9cce5c9443e917d30b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:10:11 +0530

    Create aassociative-array-var_dump.php

---
+++ 20210523/aassociative-array-var_dump.php
@@ -0,0 +1,12 @@
+<?php
+#https://paiza.io/projects/no0hz2rvz8H4V4HHnP0NQw
+//"shubham" is key and '20' is value.
+    $age = [
+        "shubham"=> 20,
+    "sajan"=>27,
+    "sunil"=>25];
+    
+    var_dump($age);
+    
+    
+?>
