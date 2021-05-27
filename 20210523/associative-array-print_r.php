2602bd8d43eaf74346f743c2361cbd07685c9b10
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:06:50 +0530

    Create associative-array-print_r.php

---
+++ 20210523/associative-array-print_r.php
@@ -0,0 +1,16 @@
+<?php
+#https://paiza.io/projects/F7WvaPG2LzmAvFIWQWMMkg
+//"shubham" is key and '20' is value.
+    $age = [
+        "shubham"=> 20,
+    "sajan"=>27,
+    "sunil"=>25];
+    
+    print_r($age);
+    
+    echo $age ["shubham"]."\n";
+     echo $age ["sajan"]."\n";
+      echo $age ["sunil"]."\n";
+       
+?>
+
