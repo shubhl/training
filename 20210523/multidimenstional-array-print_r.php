6ced2fcfedf1fd498c2f6039e66e2bdb26b87c1e
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 18:34:40 +0530

    Create multidimenstional-array-print_r.php

---
+++ 20210523/multidimenstional-array-print_r.php
@@ -0,0 +1,18 @@
+<?php
+// Your code here!
+ $emp = [
+        
+         [1,"shubham","manager",30000],
+        [2,"sunil","salesman",20000],
+        [3,"sachin","worker",15000],
+        [4,"anil","driver",8000]
+    
+        ];
+        
+        //by using print_r
+       
+        
+        print_r ($emp);
+        
+        
+?>
