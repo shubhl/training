36c9399d8451498b2fca889dcdd916ce4fd91a38
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:00:59 +0530

    Create associative-array.php

---
+++ 20210523/associative-array.php
@@ -0,0 +1,13 @@
+<?php
+#https://paiza.io/projects/RFxUF2RbIWekcN82hhCAgQ
+//"shubham" is key and '20' is value.
+    $age = array (
+        "shubham"=> 20,
+    "sajan"=>27,
+    "sunil"=>25);
+    
+    echo $age ["shubham"]."\n";
+     echo $age ["sajan"]."\n";
+      echo $age ["sunil"]."\n";
+       
+?>
