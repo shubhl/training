b076f7f8d03cd92dec1247a276f260d0922596d1
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:19:37 +0530

    Create associative-array-chnaging-value-anytime.php

---
+++ 20210523/associative-array-chnaging-value-anytime.php
@@ -0,0 +1,16 @@
+<?php
+#https://paiza.io/projects/XFDwq4pSSYfXgM379zZRdA
+//"shubham" is key and '20' is value.
+//changing values and keys 
+    $age = [
+        1=> "20",//string 
+    2=>27.3,//float
+    3=>25];//integer
+    
+    $age [1]= 23;//use to change the value of key
+    
+    "<pre>";
+    var_dump($age);
+    "<\pre>";
+    
+?>
