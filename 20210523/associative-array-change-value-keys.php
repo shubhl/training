e96bc87df7ec474bd0db2323322172c03f98fe71
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:14:09 +0530

    Create associative-array-change-value-keys.php

---
+++ 20210523/associative-array-change-value-keys.php
@@ -0,0 +1,13 @@
+<?php
+#https://paiza.io/projects/no0hz2rvz8H4V4HHnP0NQw
+//"shubham" is key and '20' is value.
+//changing values and keys 
+    $age = [
+        1=> "20",//string 
+    2=>27.3,//float
+    3=>25];//integer
+    
+    var_dump($age);
+    
+    
+?>
