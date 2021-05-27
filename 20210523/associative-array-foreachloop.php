cc1308c1335d1d33aa075c88eb2c01d86a0878bb
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 17:34:07 +0530

    Create associative-array-foreachloop.php

---
+++ 20210523/associative-array-foreachloop.php
@@ -0,0 +1,12 @@
+<?php
+#https://paiza.io/projects/WlFlU3IDWNlTIVsjiUoJIA
+//array with for each loop 
+    $age =[
+        "shubhya"=>21,
+        "rohit"=>22,
+        "sunil"=>30
+        ];
+        foreach ($age as $name){
+            echo $name."<br>";
+        }
+?>
