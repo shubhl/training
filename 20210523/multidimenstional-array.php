20025816352f21823d06259046986e18235b77cd
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-27 18:21:21 +0530

    Create multidimenstional-array.php

---
+++ 20210523/multidimenstional-array.php
@@ -0,0 +1,15 @@
+<?php
+#https://paiza.io/projects/sf5C2GjaS5CZLPzfnBdfzQ
+//this is very lengthy code we need use loops for making it easy .
+    $emp = array (
+        
+        array (1,"shubham","manager",30000),
+        array(2,"sunil","salesman",20000),
+        array(3,"sachin","worker",15000),
+        array(4,"anil","driver",8000)
+    
+        );
+        echo $emp [1][0]. " ";
+         echo $emp [1][1]." ";
+          echo $emp [1][2]." ";
+           echo $emp [1][3]." ";
