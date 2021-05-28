1530bd456314a4511665b4d96ad3030c068c5506
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 14:44:47 +0530

    Create array-searching-function.php

---
+++ 20210523/array-searching-function.php
@@ -0,0 +1,9 @@
+<?php
+#https://paiza.io/projects/NP0XCL7zigkdik6d38U8ew
+// by using for loop
+    $food = array ("orange ","banana","apple"
+        );
+        
+        echo in_array("banana",$food);
+?>
+
