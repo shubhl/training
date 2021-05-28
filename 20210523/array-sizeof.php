deb56b0a244ff00b97000060e9149bdfb908d304
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 13:51:03 +0530

    Create array-sizeof.php

---
+++ 20210523/array-sizeof.php
@@ -0,0 +1,12 @@
+<?php
+#https://paiza.io/projects/D3UvvyqKCXE4sXNV6c6Tmg
+// sizeof with multidimentional array
+    $food = array (
+        
+        '$fruits' =>array("orange ","banana","apple"), 
+        '$vegge'=>array("caarot","colard","pea")
+        );
+        
+        echo sizeof($food,1);//by using modd 
+?>
+
