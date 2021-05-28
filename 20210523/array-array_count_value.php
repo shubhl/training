ea139e742c7f69b7f1b240591d2679c238bb394b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 13:58:00 +0530

    Create array-array_count_value.php

---
+++ 20210523/array-array_count_value.php
@@ -0,0 +1,9 @@
+<?php
+#https://paiza.io/projects/dyurIRpniCqc24otlryCxw
+// array_count_values function
+    $food = array ("orange ","banana","apple"
+        );
+        
+        print_r (array_count_values($food));//by using modd 
+?>
+
