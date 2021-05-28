35939a1bc0f1ad0b9cfd46e623fba7cd737d19df
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 22:30:04 +0530

    Update date-function.php

--- 20210527/date-function.php
+++ 20210527/date-function.php
@@ -3,8 +3,25 @@
  echo "today is " .date("d")."<br>";
  //by using s code
  echo "Today is ".date ("dS")."<br>";
+ //by using week function 
+ echo "today weekday is ".date ("D")."<br>";//weekdays short form
+ 
+  echo "today weekday is ".date ("l")."<br>";//weekdays fullform
+  
+   echo "today weekday is ".date ("WS")."<br>";// (W) is use for showing weekdays in number. it start from 0 
+   
+    echo "today weekday is ".date ("D")."<br>";// (N) is use for showing weekdays in number. it start from 1
  //by using month function 
  echo "this month is ".date("F")."<br>";
+
+ echo "this month is ".date("M")."<br>";
+ 
+ echo "this month is ".date ("m")."<br>";
+ 
+ echo "this month is ". date ("n"),"<br>";
+ 
  //by using year function 
- echo "this year is ".date ("Y");
+ 
+ echo "this year is ".date ("Y")."<br>";
+ echo "this year is ".date("y") ;// small y is showing only last number of year 
 ?>
