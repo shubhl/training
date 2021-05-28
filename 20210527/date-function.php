43720e9610c4357fb010b97ad9750629453ea62b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 22:06:27 +0530

    Create date-function.php

---
+++ 20210527/date-function.php
@@ -0,0 +1,10 @@
+<?php
+#https://paiza.io/projects/w0LK2CW-YK7Rb1dGkgt1Pw
+ echo "today is " .date("d")."<br>";
+ //by using s code
+ echo "Today is ".date ("dS")."<br>";
+ //by using month function 
+ echo "this month is ".date("F")."<br>";
+ //by using year function 
+ echo "this year is ".date ("Y");
+?>
