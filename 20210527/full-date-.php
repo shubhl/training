1505fb548e90066e85efb8e2c51cb7c912390b4b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 23:14:43 +0530

    Create full-date-.php

---
+++ 20210527/full-date-.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/aYKbtjf8JweJeKIusI1DNA?language=php
+     echo "Full date is ".date("d/m/Y")."<br>";
+     echo "Full date is ".date ("d/M/Y")."<br>";
+    echo "full date is ".date("Y/M/d")."<br>";//another format of the date
+    echo "full date is ".date("Y-M-d")."<br>";
+    echo "full date is ".date("Y.M.d")."<br>";
+    echo "day in the year ".date("z")."<br>";
+    echo "week of the year ".date("t")."<br>";//for 365 days
+    echo "is this leap year ".date("L");//for leap year
+?>
