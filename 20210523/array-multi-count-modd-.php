a2023c50ba73d8f82ad03832796564516592ed8f
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 13:46:01 +0530

    Create array-multi-count-modd-.php

---
+++ 20210523/array-multi-count-modd-.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/LwZ1xxuYrnVW1_hPXmArgQ
+// count with multidimentional array
+    $food = array (
+        
+        '$fruits' =>array("orange ","banana","apple"), 
+        '$vegge'=>array("caarot","colard","pea")
+        );
+        
+        echo count ($food,1);//by using modd 
+?>
