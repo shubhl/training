1f0779ebf8acf0d16626f5dd20ce6dc2cbbf44ae
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 14:50:17 +0530

    Create array-searching-with-ife.php

---
+++ 20210523/array-searching-with-ife.php
@@ -0,0 +1,13 @@
+<?php
+#https://paiza.io/projects/NP0XCL7zigkdik6d38U8ew
+    $food = array ("orange ","banana","apple"
+        );
+        
+        //by using if else
+        if (in_array("apple",$food)) {
+            echo "function find successfully";
+        }else{
+            echo "cant find";
+        }
+?>
+
