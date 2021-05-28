67fe800f17f2b60556bc7bb3fea1b02ad94085a4
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 14:09:43 +0530

    Create array-count-loop.php

---
+++ 20210523/array-count-loop.php
@@ -0,0 +1,14 @@
+<?php
+#https://paiza.io/projects/dyurIRpniCqc24otlryCxw
+// by using for loop
+    $food = array ("orange ","banana","apple"
+        );
+        
+        $len=count($food);
+        
+        for($i =0;$i< 3;$i++){
+            echo $food[$i] ."<br>";
+        }
+        
+?>
+
