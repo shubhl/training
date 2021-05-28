b231746bcc664d692970987b49f11ba2956b7916
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 15:23:34 +0530

    Create array_replace.php

---
+++ 20210523/array_replace.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/6s-nVmsfKa182mImgsNT0g?language=php
+//indexed array with arrray_rpalce function
+    $fruit = ["apple","banana","orange"];
+    $food= ["omlet","panipuri"];
+    
+    $newarray=array_replace($fruit,$food);
+     
+     
+     print_r($newarray);
+?>
