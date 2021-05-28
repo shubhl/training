e4077b02ec181697f64316704327a284e24f2721
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-28 14:54:18 +0530

    Create array_search-function.php

---
+++ 20210523/array_search-function.php
@@ -0,0 +1,8 @@
+<?php
+#https://paiza.io/projects/h8RGzKJR33f24dKeCOBNFw?language=php
+    $food = array ("orange ","banana","apple","33");
+        //by using array_search function
+        echo array_search(33,$food);
+        
+?>
+
