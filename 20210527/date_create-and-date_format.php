f558ed8fc7cb8a4d6830b1f27463a64822267bab
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-29 21:32:56 +0530

    Update date_create-and-date_format.php

--- 20210527/date_create-and-date_format.php
+++ 20210527/date_create-and-date_format.php
@@ -5,6 +5,9 @@
    echo date_format($date,"d/m/y")."<br>";
    //date create full date with time 
    $dt=date_create ("2029-08-5 4:14:59");
-   echo date_format($dt,"D/m/y H:i:sa");
+   echo date_format($dt,"D/m/y H:i:sa")."<br>";
+   //date create function with old date time
+   $odt=date_create("2006-07-5 11:23:50");
+   echo date_format($odt,"D/mS/Y H:i:sA");
     
 ?>
