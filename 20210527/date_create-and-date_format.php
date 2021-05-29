0dbcc4941a3723df4813792eac4f6930bf2da19b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-29 21:26:15 +0530

    Create date_create-and-date_format.php

---
+++ 20210527/date_create-and-date_format.php
@@ -0,0 +1,10 @@
+<?php
+#https://paiza.io/projects/zP4N0vh6XIuyyjnTguBZGw?language=php
+// date create full date
+    $date=date_create("2023-04-15");
+   echo date_format($date,"d/m/y")."<br>";
+   //date create full date with time 
+   $dt=date_create ("2029-08-5 4:14:59");
+   echo date_format($dt,"D/m/y H:i:sa");
+    
+?>
