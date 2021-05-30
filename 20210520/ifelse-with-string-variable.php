59a31f7fd75473f19b8fb90f001a3456efafc356
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 22:46:43 +0530

    Create ifelse-with-string-variable.php

---
+++ 20210520/ifelse-with-string-variable.php
@@ -0,0 +1,24 @@
+<?php
+#https://paiza.io/projects/TxE233bK957XXoGatF2jfQ?language=php
+// if equal to
+    $name = "shubham";
+    $gen = "male ";
+    
+    if ($name == "shubham"){
+        echo "hello mr ";
+        
+    }else {
+        echo "hello miss ";
+    }
+    echo"<br>";
+    //if  not equal
+    $name = "shubham";
+    $gen = "male ";
+    
+    if ($name != "shubham"){
+        echo "hello mr ";
+        
+    }else {
+        echo "hello miss ";
+    }
+?>
