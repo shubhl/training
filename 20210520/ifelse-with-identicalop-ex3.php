e8a7a0af653865af01a28fe864f58bae5a85d9ba
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 22:38:22 +0530

    Create ifelse-with-identicalop-ex3.php

---
+++ 20210520/ifelse-with-identicalop-ex3.php
@@ -0,0 +1,23 @@
+<?php
+#https://paiza.io/projects/PIrY6gC5oHonNfJ2-m03CQ?language=php
+// identical operators
+      $y = 100;
+     
+     if ($y ===100){
+         echo "this is big";
+         
+     }else {
+         echo  "this is small";
+     }
+     echo "<br>";
+     //when value is not equal   
+     $y = 100;
+     
+     if ($y ===200){
+         echo "this is big";
+         
+     }else {
+         echo  "this is small";
+     }
+         
+?>
