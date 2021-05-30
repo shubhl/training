5e06bbd879fde8fdd3965a132768e7b32232cad5
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 19:44:04 +0530

    Create php-with-html.php

---
+++ 20210519/php-with-html.php
@@ -0,0 +1,7 @@
+<?php
+#https://paiza.io/projects/N8Azb9xj3VZX4K9Az-UyAg
+ 
+    echo "hello students <br>";
+    echo "<h1> hello sir</h1>";// for bolding the statement
+    echo "hello ", "my ", "name " , "is ", "abc ", "<br>";//this string with multiple parameters
+?>
