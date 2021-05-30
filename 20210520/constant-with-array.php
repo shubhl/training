fe6b444084b887fb791bd580cfe3a1f9ce016c65
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 21:40:25 +0530

    Create constant-with-array.php

---
+++ 20210520/constant-with-array.php
@@ -0,0 +1,7 @@
+<?php
+#https://paiza.io/projects/b8g-KMxDDb4zILz0-HMe_g?language=php
+    define ("cars",["BMW","HONDA","SUZUKI","TATA"]);
+    echo cars[1];
+    echo "<br>";
+    echo cars[3];
+?>
