27afae9a78242eb2143c40d65056374f1550b280
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 22:24:53 +0530

    Create ifelse-stmt.php

---
+++ 20210520/ifelse-stmt.php
@@ -0,0 +1,22 @@
+<?php
+#https://paiza.io/projects/QDt7UgzeaJ5W4WusW2hFxQ?language=php
+//true 
+    $x = 25;
+    if ($x>20){
+    	echo "have nice day";
+    }else{
+    		echo "have bad day "."<br>";
+    		
+    }
+    echo "<br>";
+    		//less than false
+    $y = 32;
+    if ($y>50){
+        echo "have good day ";
+        
+    }else{
+        echo "have a very bad day ";
+        
+    }
+
+?>
