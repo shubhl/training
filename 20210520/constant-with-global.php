844cf4160d679992324b18630c40ab5b735da146
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 22:15:51 +0530

    Create constant-with-global.php

---
+++ 20210520/constant-with-global.php
@@ -0,0 +1,10 @@
+<?php
+#https://paiza.io/projects/epN1Y9KSm-_cwYZsznX-cg?language=php
+    define ("hello","good night ");
+    
+    function myTest(){
+        echo hello;
+        
+    }
+    myTest();
+?>
