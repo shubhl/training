1580854861842693de42209125b2a8067aff84b4
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 20:15:46 +0530

    Create str_replace.php

---
+++ 20210519/str_replace.php
@@ -0,0 +1,7 @@
+<?php
+#https://paiza.io/projects/AxlWZO2XOC_cUJHubKDPvA?language=php
+// replace the sir word with student 
+    echo str_replace ("sir", "STUDENT", "hello sir")."<br>";
+    
+    echo strpos ("hello sir good morning","good",);
+?>
