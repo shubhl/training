1057e96c3c1cecf5e9d5e6fc084f96a167bebc6c
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 20:11:50 +0530

    Create strpos.php

---
+++ 20210519/strpos.php
@@ -0,0 +1,11 @@
+<?php
+#https://paiza.io/projects/ViwkYRDTRVsXe9yk8Orx5Q?language=php
+    echo strpos("Hell0 my friend!", "friend")."<br>";
+    // outputs 6
+    //strpos with variable 
+    $txt1 = "my name is, my self is , i am from ";
+     $txt2 = " i am , from ";
+     echo strpos ($txt1, $txt2);
+?>
+
+
