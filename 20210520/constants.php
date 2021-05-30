e6e2e367ed68c853b670f3967e4247c7979ff3d5
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 21:35:40 +0530

    Create constants.php

---
+++ 20210520/constants.php
@@ -0,0 +1,8 @@
+<?php
+#https://paiza.io/projects/IKoFMvm6TZ0EqDNyLfMi4A?language=php
+    define ("thanks","<h1>www.php.com</h1>");//constant with case sensetive 
+    echo thanks;
+    define ("hello","my name is xyz");
+    echo hello;
+  
+?>
