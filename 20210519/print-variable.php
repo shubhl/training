883fdb81f02dc34999292f86b80c9ea89de386a3
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 19:54:09 +0530

    Create print-variable.php

---
+++ 20210519/print-variable.php
@@ -0,0 +1,13 @@
+<?php
+// Your code here!
+    $txt1 = "learn php ";
+    $txt2= "php.com";
+    $a = 22;
+    
+    $b =32;
+     
+     print "<h1> $txt1 </h1> <br>";
+     print " study php at .$txt2 <br>";
+     print $a +$b ;
+    
+?>
