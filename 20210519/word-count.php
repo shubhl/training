299f1ffa3d230c2a9fb75277b7aff90568a3283a
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 21:21:34 +0530

    Create word-count.php

---
+++ 20210519/word-count.php
@@ -0,0 +1,8 @@
+<?php
+#https://paiza.io/projects/-h29iz7R54E8gArUYv7xqw?language=php
+    echo str_word_count("hello everyon")."<br>";//this is string word count
+    //word count with variable
+    $w = "hello good morning ";
+    
+    echo str_word_count($w);
+?>
