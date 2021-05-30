2c5ea8897216f3f256bf91c75e1bc8b92b4740d6
shubhl <84661383+shubhl@users.noreply.github.com>
2021-05-30 22:32:38 +0530

    Create ifelse-ex2.php

---
+++ 20210520/ifelse-ex2.php
@@ -0,0 +1,23 @@
+<?php
+#https://paiza.io/projects/tTK4cdYjqSpF53IBZ8j4Nw?language=php
+    $x = 100;
+     
+     if ($x ==100){
+         echo "this is true ";
+        
+     }else{
+         echo "this is false";
+         
+     }
+       echo "<br>";
+     //false statement
+     $y = 100;
+     
+     if ($y !=100){
+         echo "this is big";
+         
+     }else {
+         echo  "this is small";
+     }
+         
+?>
