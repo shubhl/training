4ac9befd5db909fc54d2358dd58688b66096360c
shubhl <84661383+shubhl@users.noreply.github.com>
2021-06-01 20:57:31 +0530

    Create operators.php

---
+++ 20210421/operators.php
@@ -0,0 +1,31 @@
+<?php]
+  #https://paiza.io/projects/-yIF0H4IGV5kFMDgTnIPTg?language=php
+// Your code here!
+    $a = 32;
+    $b = 43;
+    //adition op
+    $c=$a+$b;
+    echo $c ."<br>";
+    //subtracion 
+    
+    $a= 54;
+    $b= 56;
+    $c= $a -$b;
+    echo $c ."<br>";
+    
+    //division 
+    $a= 78;
+    $b =89;
+    $c = $a / $b;
+    echo $c ."<br>";
+    //percentage modulos
+    $a= 34;
+    $b=54;
+    $c=$a%$b;
+    echo $c."<br>";
+    //multiplication
+    $a= 34;
+    $b=54;
+    $c=$a*$b;
+    echo $c;
+?>
