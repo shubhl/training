c519b03ace39353c96e9bcdbcbf009bcc482662b
shubhl <84661383+shubhl@users.noreply.github.com>
2021-06-01 21:07:10 +0530

    Create functionex.php

---
+++ 20210421/functionex.php
@@ -0,0 +1,18 @@
+<?php
+#https://paiza.io/projects/WAkBXXtUK9uYR2p0BAlMyw?language=php
+    function myname($a ,$b){
+        $c=$a+$b;
+        
+        return $c;
+    }
+     echo "<h2>the sum of a and b :</h2>".myname(34,65)."<br>";
+     //
+    
+    function name(){
+    	echo "hello everyone<br>";
+     echo "my name is shubham<br>";
+    }
+    name();
+    echo "where are you from";
+?>
+
