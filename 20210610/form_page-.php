278cf4d8438e078bc6b8b0dc5b8adb9a83e39650
shubhl <84661383+shubhl@users.noreply.github.com>
2021-06-13 22:32:23 +0530

    Create form_page-.php

---
+++ 20210610/form_page-.php
@@ -0,0 +1,24 @@
+#https://paiza.io/projects/k0duqnR8JXCwFuDHzWVsww?language=php
+    <!DOCTYPE HTML>
+    <html>  
+    <body>
+    <h1>Registration Form</h1>
+    <form action="File1.php" method="post">
+    First Name: <input type="text" name="name"><br>
+    Lastname: <input type="text" name="surname"><br>
+    Gender <input type =radio name ="age">Male
+     <input type =radio name ="age">Female<br>
+     Age <input type ="number" name ="age"><br>
+    contact number <input type= integer name= "contact number"><br>
+    <input type="submit">
+    </form>
+    
+
+    </body>
+    </html>
+    
+
+
+
+
+  
