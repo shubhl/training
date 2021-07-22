4dcf63ed4bc39a2802d593a64134603a0a095be8
shubhl <84661383+shubhl@users.noreply.github.com>
2021-07-22 21:35:59 +0530

    Update newform.h

--- 20210702/newform.h
+++ 20210702/newform.h
@@ -1,12 +1,11 @@
-
 <!DOCTYPE HTML>
 <html>  
 	<body>
 		<h1>Form</h1>
 		<form action="formoutput.php" method="POST"/>
-			Name: <input type="text" name="name" placeholder ="only alphabets" required><br><br>
+			Name: <input type="text" name="name" pattern="[a-zA-Z]" placeholder ="only alphabets" required><br><br>
 			
-			Email: <input type="text" name="email" pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"  size="15" placeholder="abc@gmail.com" required /><br><br>
+			Email: <input type="text" name="email" pattern="[a-zA-Z-0-9]+@gmail.com" size="15" placeholder="abc@gmail.com" required /><br><br>
 			
 			Phonenumber <input type="text" name="number" pattern="[6-9]{1}[0-9]{9}" maxlength="10" size="10" placeholder="ig.8323423243" required /><br><br>
 			
@@ -30,4 +29,4 @@
 	</body>
 </html>
 
---------------------------------------------------------------------
+
